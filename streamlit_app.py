f24ff336c9142515f0dd988ea8ed4f11f3b4d797
J. R. Masias <131043700+JRMasias@users.noreply.github.com>
2024-11-12 12:25:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🤖 Machine Learning App For Project 5')
 
 st.info('This is a machine learning app for project 5')
 
-df = pd.read_csv('https://raw.githubusercontent.com/JRMasias/ML_P5_Streamlit/refs/heads/master/dataset.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/JRMasias/ML_P5_Streamlit/refs/heads/master/dataset.csv')
+  df
