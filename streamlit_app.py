74c7543fb4deb8182d1675338d12e193a3123ea2
J. R. Masias <131043700+JRMasias@users.noreply.github.com>
2024-11-12 14:33:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ with st.expander('Data'):
 # Data Visualization
 with st.expander('Data Visualization'):
     st.write('All Data')
-    st.scatter_chart(df)
+    st.scatter_chart(data=df, x=X_raw, y='Turnout')
     
     st.write('VEP & Turnout')
     st.scatter_chart(data=df, x='Voting Eligible Population (VEP)', y='Registered Voters', color='Turnout')
